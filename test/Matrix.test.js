0d22c730229edc9bf41dc5583ce1a597ca514c0e
Merline Belizaire <merline@noukod.com>
2019-04-30 07:30:28 +0000

    Automatically backed up by Learn

--- test/Matrix.test.js
+++ test/Matrix.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
 // import sinon from 'sinon';
-
+//
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
